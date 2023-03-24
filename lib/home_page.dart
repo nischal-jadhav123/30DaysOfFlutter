b17be6aeddac2e44b9c14b46a44ca401c6bef021
nischal-jadhav123 <nischal.jadhav@gmail.com>
2023-03-25 03:35:24 +0530

    Day 2 added Botttom Nav bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,35 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int val = 20;
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Hello World"),
-        ),
-      ),
+      body: val == 10
+          ? Center(child: CircularProgressIndicator())
+          : Center(
+              child: Container(
+                child: Text("Hello World"),
+              ),
+            ),
       drawer: Drawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'School',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+        ],
+      ),
     );
   }
 }
